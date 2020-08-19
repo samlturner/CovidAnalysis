51d0e81e28d1ab78ceac40d4bcc75ba93f69e92f
Samuel Turner <samt5@vt.edu>
2020-08-19 14:56:28 -0400

    Commentary and formatting updates

--- plot_va_cases.py
+++ plot_va_cases.py
@@ -6,24 +6,29 @@ matplotlib.use('agg')
 import matplotlib.pyplot as plt
 from sodapy import Socrata
 
+# Useful constants
 va_pop = 8535519
 today = 19
 month = 8
 
 # df = pd.read_csv("VDH-COVID-19-PublicUseDataset-Cases.csv")
 
+# Read in the data from the VA government API
 client = Socrata("data.virginia.gov", None)
 results = client.get("bre9-aqqr", limit=10000)
 df = pd.DataFrame.from_records(results)
 df.head()
 
+# Define output arrays
 prop_arr = []
 averages_arr = []
 all_days = [0] * 36
+# For a period of 30 days
 for j in reversed(range(30)):
     tomorrow = today + 1 - j
     days = []
     num_days = 7
+    # For each day in the range, compute the total number of new cases for that day
     for i in range(tomorrow - num_days,tomorrow):
         prev_day = i-1
         curr_day = i
@@ -44,20 +49,20 @@ for j in reversed(range(30)):
         previous = df[df['report_date']=='2020-0'+str(prev_month)+'-'+prev_day_str+'T00:00:00.000']['total_cases'].astype('int32').sum()
         current = df[df['report_date']=='2020-0'+str(curr_month)+'-'+curr_day_str+'T00:00:00.000']['total_cases'].astype('int32').sum()
         days.append(current - previous)
-
+    # Compute the relevant statistics for this range
     sum = 0
     for i in range(len(days)):
-        all_days[abs(29-j)+i] = days[i]
+        all_days[abs(29-j)+i] = days[i] # Save each day's individual statistics (TODO: pretty inefficient)
         sum += days[i]
     average = sum / (num_days * 1.0)
     proportion = average / va_pop
     prop_per_hthousand = round(proportion * 100000,2)
     prop_arr.append(prop_per_hthousand)
 
+# Plot the number of cases per day
 plt.clf()
 plt.figure(num=None,figsize=(18,6),dpi=80,facecolor='w',edgecolor='k')
 xlist = list(range(0,36))
-
 plt.plot(xlist,all_days,'bo-')
 for x,y in zip(xlist,all_days):
     label = "{:d}".format(y)
@@ -66,15 +71,13 @@ for x,y in zip(xlist,all_days):
                 textcoords="offset points",
                 xytext=(0,10),
                 ha='center')
-
 plt.ylim(ymin=0)
 plt.savefig("graph_perday.png")
 
-
+# Plot the 7-day rolling average of cases per 100,000
 plt.clf()
 plt.figure(num=None,figsize=(18,6),dpi=80,facecolor='w',edgecolor='k')
 xlist = list(range(0,30))
-
 plt.plot(xlist,prop_arr,'bo-')
 for x,y in zip(xlist,prop_arr):
     label = "{:.2f}".format(y)
@@ -83,7 +86,5 @@ for x,y in zip(xlist,prop_arr):
                 textcoords="offset points",
                 xytext=(0,10),
                 ha='center')
-
 plt.ylim(0,15)
-
 plt.savefig("graph_perhthou.png")
